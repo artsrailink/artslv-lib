525170fd4603361e7598ab78eb61e8572f31306d
Irpan Budiana <64187@kereta-api.co.id>
2016-10-11 12:33:05 +0700

    activitylog

--- src/main/java/co/id/artslv/lib/raws/Activitylog.java
+++ src/main/java/co/id/artslv/lib/raws/Activitylog.java
@@ -3,7 +3,13 @@
  */
 package co.id.artslv.lib.raws;
 
-import java.io.Serializable;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 import javax.persistence.Basic;
 import javax.persistence.Column;
@@ -12,69 +18,63 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 
 /**
  * @author root
  */
 @Entity
 @Table(name = "arts_t_activitylog")
-public class Activitylog implements Serializable {
+public class Activitylog {
 
-    @Column(name = "c_activitylog_id", table = "arts_t_activitylog", nullable = false, length = 36)
+    @Column(name = "c_activitylog_id")
     @Id
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
 
-    @Column(name = "c_activitylog_object", table = "arts_t_activitylog", nullable = false, length = 50)
-    @Basic
+    @Column(name = "c_activitylog_object")
     private String cActivitylogObject;
 
-    @Column(name = "c_activitylog_objectid", table = "arts_t_activitylog", nullable = false, length = 36)
-    @Basic
+    @Column(name = "c_activitylog_objectid")
     private String cActivitylogObjectid;
 
-    @Column(name = "c_activitylog_action", table = "arts_t_activitylog", nullable = false, length = 50)
-    @Basic
+    @Column(name = "c_activitylog_action")
     private String cActivitylogAction;
 
-    @Column(name = "c_activitylog_date", table = "arts_t_activitylog", nullable = false)
-    @Basic
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date cActivitylogDate;
+    @Column(name = "c_activitylog_date")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @Type(type = "org.hibernate.type.LocalDateType")
+    private LocalDate cActivitylogDate;
 
-    @Column(name = "c_activitylog_value", table = "arts_t_activitylog", nullable = false, length = 2147483647)
+    @Column(name = "c_activitylog_value")
     @Lob
-    @Basic
     private String cActivitylogValue;
 
-    @Column(name = "c_activitylog_domain", table = "arts_t_activitylog", nullable = false, length = 36)
-    @Basic
+    @Column(name = "c_activitylog_domain")
     private String cActivitylogDomain;
 
-    @Column(name = "c_user_id", table = "arts_t_activitylog", nullable = false, length = 36)
-    @Basic
+    @Column(name = "c_user_id")
     private String cUserId;
 
-    @Column(name = "c_wslog_id", table = "arts_t_activitylog", length = 36)
-    @Basic
+    @Column(name = "c_wslog_id")
     private String cWslogId;
 
-    @Column(name = "c_unit_id", table = "arts_t_activitylog", nullable = false, length = 36)
-    @Basic
+    @Column(name = "c_unit_id")
     private String cUnitId;
 
-    @Column(name = "c_activitylog_createdby", table = "arts_t_activitylog", nullable = false, length = 36)
-    @Basic
+    @Column(name = "c_activitylog_createdby")
     private String cActivitylogCreatedby;
 
-    @Column(name = "c_activitylog_createdon", table = "arts_t_activitylog", nullable = false)
-    @Basic
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date cActivitylogCreatedon;
+    @Column(name = "c_activitylog_createdon")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Type(type = "org.hibernate.type.LocalDateTimeType")
+    private LocalDateTime cActivitylogCreatedon;
 
     public String getId() {
         return id;
@@ -108,14 +108,15 @@ public class Activitylog implements Serializable {
         this.cActivitylogAction = cActivitylogAction;
     }
 
-    public Date getcActivitylogDate() {
+    public LocalDate getcActivitylogDate() {
         return cActivitylogDate;
     }
 
-    public void setcActivitylogDate(Date cActivitylogDate) {
+    public void setcActivitylogDate(LocalDate cActivitylogDate) {
         this.cActivitylogDate = cActivitylogDate;
     }
-
+    
+    
     public String getcActivitylogValue() {
         return cActivitylogValue;
     }
@@ -164,13 +165,11 @@ public class Activitylog implements Serializable {
         this.cActivitylogCreatedby = cActivitylogCreatedby;
     }
 
-    public Date getcActivitylogCreatedon() {
+    public LocalDateTime getcActivitylogCreatedon() {
         return cActivitylogCreatedon;
     }
 
-    public void setcActivitylogCreatedon(Date cActivitylogCreatedon) {
+    public void setcActivitylogCreatedon(LocalDateTime cActivitylogCreatedon) {
         this.cActivitylogCreatedon = cActivitylogCreatedon;
     }
-    
-    
 }
