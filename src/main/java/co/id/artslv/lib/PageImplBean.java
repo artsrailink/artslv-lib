3d78730128fc4aad46bbaeb3e6c0755c5e6be1c7
Irpan Budiana <64187@kereta-api.co.id>
2016-10-11 15:56:45 +0700

    edit

--- src/main/java/co/id/artslv/lib/PageImplBean.java
+++ src/main/java/co/id/artslv/lib/PageImplBean.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */package co.id.artslv.lib;
+package co.id.artslv.lib;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import java.util.ArrayList;
